6e736db98ef861fb935b797f0b0fb5a29d657de9
Bráulio Bezerra <brauliobezerra@gmail.com>
2021-12-25 09:59:38 -0300

    tracer: move to its own modules

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,45 @@
 mod camera;
 mod geometry;
 mod object;
+mod scene;
+mod tracer;
 
-use std::{f64::consts::PI, fs::File, io::BufWriter};
-
-use camera::Camera;
-use geometry::{Intersect, Ray};
-use glam::DVec3;
-use object::sphere::Sphere;
+use std::{fs::File, io::BufWriter};
 
 fn main() {
-    // configs
+    // env_logger::init();
+
+    let scene = &scene::icosahedron();
+
+    let max_reflections = 5;
+    let samples_per_pixel = 256;
     let frames = 1;
     let num_threads = 16;
-    let samples_per_pixel = 4096;
-    let (x_res, y_res) = (16 * 16 * 4, 16 * 16 * 4);
-    let fov = 60.0 * 2.0 * PI / 360.0;
-    let max_reflections = 10;
     let gamma_correction = 1.0 / 2.0;
+    let (x_res, y_res) = (16 * 16, 16 * 16);
 
-    // scene objects
-    let scene = &vec![
-        Sphere::new((0.1, 0.1, 0.1), 1.5),
-        Sphere::new((3.0, 3.0, 0.0), 1.0),
-        Sphere::new((3.0, -2.0, 0.0), 2.0),
-        Sphere::new((0.0, 0.0, 100.0), 80.0),
-    ];
-
-    // lights
-    let lights = &vec![
-        Sphere::new((-8000.0, 0.0, 0.0), 4900.0),
-        Sphere::new((10.5, 1.5, -1.0), 0.3),
-        Sphere::new((-10.0, -3.0, -1.0), 0.3),
-    ];
-
-    // camera
-    let mut camera = Camera::new(
-        DVec3::new(1.0, 0.0, -8.0),
-        DVec3::new(0.0, 0.0, 1.0).normalize(),
-        0.0,
-        fov,
-        fov * (y_res as f64 / x_res as f64),
-        0.1,
-    );
+    let mut image = vec![0.0; x_res * y_res];
 
     for frame in 0..frames {
-        let mut image = vec![0.0; x_res * y_res];
+        tracer::render(
+            scene,
+            x_res,
+            y_res,
+            num_threads,
+            samples_per_pixel,
+            max_reflections,
+            &mut image,
+        );
 
-        let y_block_size = y_res / num_threads;
-
-        // iterate rays to calculate pixels
-
-        let cam_ref = &camera;
-        let _ = crossbeam::scope(|scope| {
-            for (thread_num, chunk) in image.chunks_mut(y_block_size * x_res).enumerate() {
-                scope.spawn(move |_| {
-                    for y in 0..y_block_size {
-                        for x in 0..x_res {
-                            let abs_x = x;
-                            let abs_y = thread_num * y_block_size + y;
-                            for _ in 0..samples_per_pixel {
-                                let ray = cam_ref.ray((abs_x, abs_y), (x_res, y_res));
-                                chunk[y * x_res + x] += (1.0 / samples_per_pixel as f64)
-                                    * ray_trace(ray, scene, lights, max_reflections + 1);
-                            }
-                        }
-                    }
-                });
-            }
-        });
-
-        // save pixels
         save_to_png(
-            format!("output-{:04}.png", frame).as_str(),
+            format!("target/output-{:04}.png", frame).as_str(),
             &image,
             x_res,
             y_res,
             gamma_correction,
         );
+
         println!("Frame {} completed", frame);
-
-        // change camera position
-        camera.origin.z -= 0.01;
-        camera.origin.y += 0.01;
-        camera.dir = (DVec3::new(0.1, 0.1, 0.1) - camera.origin).normalize();
-    }
-}
-
-fn ray_trace(ray: Ray, scene: &[Sphere], lights: &[Sphere], remaining_steps: usize) -> f64 {
-    // find intersections
-    let mut vec = vec![];
-    for obj in scene {
-        if let Some(normal) = obj.intersect(ray) {
-            vec.push(((normal.origin - ray.origin).length_squared(), normal, false));
-        }
-    }
-    for light in lights {
-        if let Some(normal) = light.intersect(ray) {
-            vec.push(((normal.origin - ray.origin).length_squared(), normal, true));
-        }
-    }
-
-    // get the nearest
-    vec.sort_by(|(dist_a, _, _), (dist_b, _, _)| {
-        dist_a
-            .partial_cmp(dist_b)
-            .unwrap_or(std::cmp::Ordering::Less)
-    });
-
-    // calculate light intensity
-    if let Some((_, _, true)) = vec.first() {
-        1.0
-    } else if let Some((_, normal, false)) = vec.first() {
-        if remaining_steps > 1 {
-            // calculate reflected ray
-            let reflected = ray.reflect(*normal);
-            0.98 * ray_trace(reflected, scene, lights, remaining_steps - 1)
-        } else {
-            0.0
-        }
-    } else {
-        0.0
     }
 }
 
---
+++ src/tracer.rs
@@ -0,0 +1,149 @@
+use log::debug;
+
+use crate::{
+    geometry::{Intersect, Ray},
+    object::sphere::Sphere,
+    scene::Scene,
+};
+
+pub fn render(
+    scene: &Scene,
+    x_res: usize,
+    y_res: usize,
+    num_threads: usize,
+    samples_per_pixel: usize,
+    max_reflections: usize,
+    image: &mut Vec<f64>,
+) {
+    let y_block_size = y_res / num_threads;
+
+    // iterate rays to calculate pixels
+
+    let _ = crossbeam::scope(|scope| {
+        for (thread_num, chunk) in image.chunks_mut(y_block_size * x_res).enumerate() {
+            scope.spawn(move |_| {
+                for y in 0..y_block_size {
+                    for x in 0..x_res {
+                        let abs_x = x;
+                        let abs_y = thread_num * y_block_size + y;
+                        for _ in 0..samples_per_pixel {
+                            let ray = scene.camera.ray((abs_x, abs_y), (x_res, y_res));
+                            chunk[y * x_res + x] += (1.0 / samples_per_pixel as f64)
+                                * trace_ray(
+                                    ray,
+                                    &scene.objects,
+                                    &scene.lights,
+                                    max_reflections + 1,
+                                );
+                        }
+                    }
+                }
+            });
+        }
+    });
+}
+
+pub fn trace_ray(
+    ray: Ray,
+    scene: &[Box<dyn Intersect>],
+    lights: &[Sphere],
+    remaining_steps: usize,
+) -> f64 {
+    debug!("tracing ray {:?}", ray);
+
+    // find intersections
+    let mut vec = vec![];
+    for obj in scene {
+        debug!("testing intersection with {:?}", obj);
+        if let Some(normal) = obj.intersect(ray) {
+            debug!(
+                "ray intersects with {:?} with distance {:.3}",
+                obj,
+                (normal.origin - ray.origin).length()
+            );
+            vec.push(((normal.origin - ray.origin).length_squared(), normal, false));
+        } else {
+            debug!("no intersection with object {:?}", obj);
+        }
+    }
+    for light in lights {
+        debug!("testing intersection with light {:?}", light);
+        if let Some(normal) = light.intersect(ray) {
+            debug!(
+                "ray intersects with light {:?} with distance {:.3}",
+                light,
+                (normal.origin - ray.origin).length()
+            );
+            vec.push(((normal.origin - ray.origin).length_squared(), normal, true));
+        } else {
+            debug!("no intersection with light {:?}", light);
+        }
+    }
+
+    // get the nearest
+    vec.sort_by(|(dist_a, _, _), (dist_b, _, _)| {
+        dist_a
+            .partial_cmp(dist_b)
+            .unwrap_or(std::cmp::Ordering::Less)
+    });
+
+    // calculate light intensity
+    if let Some((_, normal, true)) = vec.first() {
+        debug!("ray reaches a light source at {}", normal.origin);
+        1.0
+    } else if let Some((_, normal, false)) = vec.first() {
+        debug!("ray reaches an object at {}", normal.origin);
+        if remaining_steps > 1 {
+            // calculate reflected ray
+            let reflected = ray.reflect(*normal);
+            debug!("reflected ray is {:?}", reflected);
+            debug!("{} remaining steps", remaining_steps - 1);
+            0.98 * trace_ray(reflected, scene, lights, remaining_steps - 1)
+        } else {
+            debug!("no more recursion available");
+            0.0
+        }
+    } else {
+        0.0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use log::debug;
+
+    use crate::{
+        geometry::{Intersect, Ray},
+        object::{sphere::Sphere, triangle::Triangle},
+        tracer::trace_ray,
+    };
+
+    // #[test]
+    #[allow(unused)]
+    fn reflect_triangle() {
+        let ray_along_z = Ray::from_to((0.0, 0.0, -1.0), (0.0, 0.0, 1.0));
+        let scene: Vec<Box<dyn Intersect>> = vec![Box::new(Triangle::from_tuples(
+            (0.0, 10.0, 0.1),
+            (10.0, -10.0, 0.0),
+            (-10.0, -10.0, 0.0),
+        ))];
+        let lights = vec![Sphere::new((0.0, 0.0, -2.0), 0.2)];
+
+        assert!(trace_ray(ray_along_z, &scene, &lights, 3) > 0.5);
+    }
+
+    #[test]
+    fn reflect_triangle2() {
+        env_logger::init();
+
+        let ray = Ray::from_to((0.0, 0.0, 8.0), (4.0, -4.0, 0.0));
+        let scene: Vec<Box<dyn Intersect>> = vec![Box::new(Triangle::from_tuples(
+            (-2.5, -4.5, 0.0),
+            (6.5, 4.5, 0.0),
+            (6.5, -4.5, 0.0),
+        ))];
+        let lights = vec![Sphere::new((4.0, 0.0, 2.0), 1.0)];
+
+        debug!("{:?}", trace_ray(ray, &scene, &lights, 10));
+    }
+}
