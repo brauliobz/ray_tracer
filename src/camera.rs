3a0235f4082cd759b7eaf4fd51eee94a40c17bee
Bráulio Bezerra <brauliobezerra@gmail.com>
2021-12-25 09:44:42 -0300

    camera: use an up vector instead of a rotation

--- src/camera.rs
+++ src/camera.rs
@@ -1,66 +1,44 @@
-use glam::{DVec3, DMat3};
+use glam::DVec3;
 use nanorand::Rng;
 
 use crate::geometry::Ray;
 
+#[derive(Debug)]
 pub struct Camera {
     pub origin: DVec3,
     pub dir: DVec3,
-    pub rotation_z: f64,
+    pub up: DVec3,
     pub x_fov: f64,
     pub y_fov: f64,
-    pub film_distance: f64,
+    pub sensor_distance: f64,
     pixel_lower_left: DVec3,
     x_vec: DVec3,
     y_vec: DVec3,
 }
 
 impl Camera {
-    pub fn new(origin: DVec3, dir: DVec3, rotation_z: f64, x_fov: f64, y_fov: f64, film_distance: f64) -> Camera {
-        let angle_x = x_fov / 2.0;
-        let angle_y = y_fov / 2.0;
-
-        let rotate_x_left = DMat3::from_cols(
-            DVec3::new(1.0, 0.0, 0.0),
-            DVec3::new(0.0, angle_x.cos(), angle_x.sin()),
-            DVec3::new(0.0, -angle_x.sin(), angle_x.cos()),
-        ).transpose();
-
-        let rotate_x_right = DMat3::from_cols(
-            DVec3::new(1.0, 0.0, 0.0),
-            DVec3::new(0.0, angle_x.cos(), -angle_x.sin()),
-            DVec3::new(0.0, angle_x.sin(), angle_x.cos()),
-        ).transpose();
-
-        let rotate_y_lower = DMat3::from_cols(
-            DVec3::new(angle_y.cos(), 0.0, -angle_y.sin()),
-            DVec3::new(0.0, 1.0, 0.0),
-            DVec3::new(angle_y.sin(), 0.0, angle_y.cos()),
-        ).transpose();
-
-        let rotate_y_top = DMat3::from_cols(
-            DVec3::new(angle_y.cos(), 0.0, angle_y.sin()),
-            DVec3::new(0.0, 1.0, 0.0),
-            DVec3::new(-angle_y.sin(), 0.0, angle_y.cos()),
-        ).transpose();
-
-        let lower_right = rotate_y_lower * rotate_x_right * dir + origin;
-        let lower_left = rotate_y_lower * rotate_x_left * dir + origin;
-        let top_left = rotate_y_top * rotate_x_left * dir + origin;
-
-        let x_vec = lower_right - lower_left;
-        let y_vec = top_left - lower_left;
+    pub fn new(
+        origin: DVec3,
+        dir: DVec3,
+        up: DVec3,
+        x_fov: f64,
+        y_fov: f64,
+        sensor_distance: f64,
+    ) -> Camera {
+        let y_vec = (y_fov / 2.0).tan() * up.normalize();
+        let x_vec = (x_fov / 2.0).tan() * up.cross(dir).normalize();
+        let lower_left = origin + dir * sensor_distance - x_vec - y_vec;
 
         Camera {
             origin,
             dir,
-            film_distance,
+            sensor_distance,
             x_fov,
             y_fov,
-            rotation_z,
+            up,
             pixel_lower_left: lower_left,
-            x_vec,
-            y_vec,
+            x_vec: x_vec * 2.0,
+            y_vec: y_vec * 2.0,
         }
     }
 
@@ -76,4 +54,41 @@ impl Camera {
             dir: dir.normalize(),
         }
     }
+}
+
+#[test]
+fn camera() {
+    let camera = Camera::new(
+        DVec3::new(0.0, 0.0, -1.0),
+        DVec3::new(0.0, 0.0, 1.0),
+        DVec3::new(0.0, 1.0, 0.0),
+        90.0f64.to_radians(),
+        90.0f64.to_radians(),
+        1.0,
+    );
+
+    dbg!(&camera);
+
+    // ray going near the center
+    assert!((camera.ray((1000, 1000), (2000, 2000)).dir.x - 0.0).abs() <= 10e-4);
+    assert!((camera.ray((1000, 1000), (2000, 2000)).dir.y - 0.0).abs() <= 10e-4);
+    assert!((camera.ray((1000, 1000), (2000, 2000)).dir.z - 1.0).abs() <= 10e-4);
+
+    // ray going near the lower left
+    let p = DVec3::new(-1.0, -1.0, 1.0).normalize();
+    assert!((camera.ray((0, 0), (2000, 2000)).dir.x - p.x).abs() <= 10e-4);
+    assert!((camera.ray((0, 0), (2000, 2000)).dir.y - p.y).abs() <= 10e-4);
+    assert!((camera.ray((0, 0), (2000, 2000)).dir.z - p.z).abs() <= 10e-4);
+
+    // ray going near the lower right
+    let p = DVec3::new(1.0, -1.0, 1.0).normalize();
+    assert!((camera.ray((2000, 0), (2000, 2000)).dir.x - p.x).abs() <= 10e-4);
+    assert!((camera.ray((2000, 0), (2000, 2000)).dir.y - p.y).abs() <= 10e-4);
+    assert!((camera.ray((2000, 0), (2000, 2000)).dir.z - p.z).abs() <= 10e-4);
+
+    // ray going near the top right
+    let p = DVec3::new(1.0, 1.0, 1.0).normalize();
+    assert!((camera.ray((2000, 2000), (2000, 2000)).dir.x - p.x).abs() <= 10e-4);
+    assert!((camera.ray((2000, 2000), (2000, 2000)).dir.y - p.y).abs() <= 10e-4);
+    assert!((camera.ray((2000, 2000), (2000, 2000)).dir.z - p.z).abs() <= 10e-4);
 }
